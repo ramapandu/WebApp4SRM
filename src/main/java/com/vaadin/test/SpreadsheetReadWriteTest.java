111ddeb130a2b4e1b1868206bf5c36dd660835f4
ramapandu <ramapandu.cheti@stud.hochschule.heidelberg.de>
2016-10-21 11:46:55 +0200

    Modified TEST CASE for ReadWrite and added sap-deal1.xlsx and sap-deal2.xlsx

--- src/main/java/com/vaadin/test/SpreadsheetReadWriteTest.java
+++ src/main/java/com/vaadin/test/SpreadsheetReadWriteTest.java
@@ -45,18 +45,23 @@ public class SpreadsheetReadWriteTest {
     public void openAndSaveFile_emptyXLSXFile_openAndSaveWorks()
             throws URISyntaxException, IOException {
         URL testSheetResource = this.getClass().getClassLoader()
-                .getResource("SAP-DEAl.xlsx");
+                .getResource("SAP-DEAl2.xlsx");
         File testSheetFIle = new File(testSheetResource.toURI());
         Spreadsheet sheet = new Spreadsheet(testSheetFIle);
-
-        File tempFile = File.createTempFile("resultEmptyFile", "xlsx");
+        sheet.getCell("A4").setCellValue("TEST SUCCESS");
+//        System.out.println(sheet.getCell("A4"));
+        File tempFile = new File("SAP-DEAl2.xlsx");
+        
         FileOutputStream tempOutputStream = new FileOutputStream(tempFile);
         sheet.write(tempOutputStream);
         tempOutputStream.close();
+        Spreadsheet sheet1 = new Spreadsheet(tempFile);
+        System.out.println(sheet1.getCell("A4"));
         tempFile.delete();
 
         // no exceptions, everything ok
     }
+
 
     @Test
     public void openAndSaveFile_emptyXLSXFile_FileDoesNotContainAdditionalDrawing()
